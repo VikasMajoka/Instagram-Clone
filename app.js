9bfac386adc1187107b3d25b8efcf71ff3b9b403
Vikas <majokavikas@gmail.com>
2024-04-13 22:09:46 +0530

    Middleware Execution

--- app.js
+++ app.js
@@ -2,8 +2,16 @@ const express = require('express')
 const app = express()
 const PORT = 5000
 
+const customMiddleware = (req ,res ,next)=>{
+    console.log("middleware executed!!")
+    next()
+}
+
+app.use(customMiddleware)
+
 
 app.get('/',(req,res)=>{
+    console.log("home")
     res.send("hello world")
 })
 
