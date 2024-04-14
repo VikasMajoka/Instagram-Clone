4b83392eb8eb2abcea3d47b62830b741d7c1dfe1
Vikas <majokavikas@gmail.com>
2024-04-14 11:01:24 +0530

    Creating User Schema

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const mongoose = require('mongoose')
 const PORT = 5000
 const {MONGOURI}= require('./keys')
 
-mongoose.connect(MONGOURI)
+require('./models/user')
+
+mongoose.connect("mongodb+srv://majokavikas:vikasmajoka@cluster0.fh63oeu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+
+
 mongoose.connection.on('connected',()=>{
     console.log("connected to mongo yeah")
 })
--- keys.js
+++ keys.js
@@ -1,3 +1,3 @@
 module.exports={
-MONGOURI:"mongodb+srv://majokavikas:<password>@cluster0.fh63oeu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGOURI:"mongodb+srv://majokavikas:vikasmajoka@cluster0.fh63oeu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 }
