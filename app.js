935d77ef67ffbf84a308f880920333d617cef47b
Vikas <majokavikas@gmail.com>
2024-04-14 12:44:39 +0530

    Postig data in database

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const PORT = 5000
 const {MONGOURI}= require('./keys')
 
 require('./models/user')
+app.use(express.json())
+app.use(require('./routes/auth'))
 
 mongoose.connect("mongodb+srv://majokavikas:vikasmajoka@cluster0.fh63oeu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
