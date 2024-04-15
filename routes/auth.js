301d1079a2885a96af838e09f42e4de749a5a750
Vikas <majokavikas@gmail.com>
2024-04-15 12:42:50 +0530

    Creating Sign in Route

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,8 @@ const router = express.Router()
 const mongoose = require('mongoose')
 const User = mongoose.model("User")
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const{JWT_SECRET} = require('../keys')
 
 router.post('/signup',(req,res)=>{
     const {name,email,password}=req.body
@@ -35,6 +37,31 @@ router.post('/signup',(req,res)=>{
     .catch(err=>{
         console.log(err)
       }) 
+})
+
+router.post('/signin',(req,res)=>{
+    const {email,password} = req.body
+    if(!email || !password){
+       return res.status(422).json({error:"please add email or password"})
+    }
+    User.findOne({email:email})
+    .then(savedUser=>{
+        if(!savedUser){
+           return res.status(422).json({error:"Invalid Email or password"})
+        }
+        bcrypt.compare(password,savedUser.password)
+        .then(doMatch=>{
+            if(doMatch){
+             //res.json({message:"successfully signed in"})
+            }
+            else{
+                return res.status(422).json({error:"Invalid Email or password"})
+            }
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+    })
 })
 
 
